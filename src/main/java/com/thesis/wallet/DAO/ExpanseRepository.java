b4b3b128a7b46c4a631d9bb0e71d4f878b1e04ff
danilashtefan <danilashtefan@gmail.com>
2021-09-08 15:33:35 +0200

    Added findByAll methods

--- src/main/java/com/thesis/wallet/DAO/ExpanseRepository.java
+++ src/main/java/com/thesis/wallet/DAO/ExpanseRepository.java
@@ -3,7 +3,8 @@ package com.thesis.wallet.DAO;
 
 import com.thesis.wallet.entity.Expanse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ExpanseRepository extends JpaRepository<Expanse,Long> {
-    Long deleteByName(String name);
+    Expanse findByName(@Param("name") String name);
 }
