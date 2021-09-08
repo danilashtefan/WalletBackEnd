38256bdf88cf792e915f8c12d1abdfcc474df106
danilashtefan <danilashtefan@gmail.com>
2021-09-08 11:02:18 +0200

    Added configuration to expose id's in JSON response

--- src/main/java/com/thesis/wallet/DAO/ExpanseRepository.java
+++ src/main/java/com/thesis/wallet/DAO/ExpanseRepository.java
@@ -5,4 +5,5 @@ import com.thesis.wallet.entity.Expanse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExpanseRepository extends JpaRepository<Expanse,Long> {
+    Long deleteByName(String name);
 }
---
+++ src/main/java/com/thesis/wallet/config/SpringDataRestConfig.java
@@ -0,0 +1,19 @@
+package com.thesis.wallet.config;
+
+import com.thesis.wallet.entity.Expanse;
+import com.thesis.wallet.entity.ExpanseCategory;
+import com.thesis.wallet.entity.Wallet;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class SpringDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Expanse.class);
+        config.exposeIdsFor(ExpanseCategory.class);
+        config.exposeIdsFor(Wallet.class);
+    }
+}
