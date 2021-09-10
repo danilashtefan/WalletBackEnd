8b96e31ca30fa005b8c922b5a6733bf3d70b916c
danilashtefan <danilashtefan@gmail.com>
2021-09-10 16:30:40 +0200

    Added ExpanseCategories Request and Response

--- src/main/java/com/thesis/wallet/DAO/ExpanseCategoryRepository.java
+++ src/main/java/com/thesis/wallet/DAO/ExpanseCategoryRepository.java
@@ -4,6 +4,6 @@ import com.thesis.wallet.entity.ExpanseCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "expanseCategory", path = "expanse-category")
+
 public interface ExpanseCategoryRepository extends JpaRepository<ExpanseCategory,Long> {
 }
