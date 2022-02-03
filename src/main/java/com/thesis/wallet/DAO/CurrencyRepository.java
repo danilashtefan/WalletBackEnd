2299af9324d0cb091e2be7951af992ec8932ffb5
danilashtefan <danilashtefan@gmail.com>
2022-02-03 12:03:42 +0200

    Deleted curreency as a separate entity

--- src/main/java/com/thesis/wallet/DAO/CurrencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thesis.wallet.DAO;
-
-import com.thesis.wallet.entity.Currency;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface CurrencyRepository extends JpaRepository<Currency,Long> {
-}
