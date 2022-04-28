86eece38b8bbdf10214141bd61ae31afd0dae1ac
danilashtefan <danilashtefan@gmail.com>
2022-04-28 10:47:12 +0300

    response changes

--- src/main/java/com/thesis/wallet/controllers/ExpenseController.java
+++ src/main/java/com/thesis/wallet/controllers/ExpenseController.java
@@ -33,70 +33,66 @@ public class ExpenseController {
     private final WalletService walletService;
 
     @GetMapping("/expanses2")
-    public ResponseEntity<List<Expense>> getAllExpenses(Authentication authentication) {
+    public List<Expense> getAllExpenses(Authentication authentication) {
         String username = (String) authentication.getPrincipal();
-        return ResponseEntity.ok().body(expenseService.getAllExpenses(username));
+        return expenseService.getAllExpenses(username);
     }
 
     @DeleteMapping("/expanses2/{id}")
-    public ResponseEntity<String> deleteByIdAndUsername(@PathVariable Long id, Authentication authentication){
+    public String deleteByIdAndUsername(@PathVariable Long id, Authentication authentication){
         String username = (String) authentication.getPrincipal();
-        return ResponseEntity.ok().body(expenseService.deleteByIdAndUsername(id, username));
+        return expenseService.deleteByIdAndUsername(id, username);
     }
 
     @GetMapping("/expanses2/{id}")
-    public ResponseEntity<Optional<Expense>> getExpense(@PathVariable Long id, Authentication authentication) {
+    public Optional<Expense> getExpense(@PathVariable Long id, Authentication authentication) {
         String username = (String) authentication.getPrincipal();
-        return ResponseEntity.ok().body(expenseService.getExpense(id, username));
+        return expenseService.getExpense(id, username);
     }
 
     @GetMapping("/expanses2/{id}/category")
-    public ResponseEntity<Optional<Category>> getCategoryForExpense(@PathVariable Long id, Authentication authentication) {
+    public Optional<Category> getCategoryForExpense(@PathVariable Long id, Authentication authentication) {
         String username = (String) authentication.getPrincipal();
-        return ResponseEntity.ok().body(expenseService.getCategoryForExpense(id, username));
+        return expenseService.getCategoryForExpense(id, username);
     }
 
     @GetMapping("/expanses2/{id}/wallet")
-    public ResponseEntity<Optional<Wallet>> getWalletForExpense(@PathVariable Long id, Authentication authentication) {
+    public Optional<Wallet> getWalletForExpense(@PathVariable Long id, Authentication authentication) {
         String username = (String) authentication.getPrincipal();
-        return ResponseEntity.ok().body(expenseService.getWalletForExpense(id, username));
+        return expenseService.getWalletForExpense(id, username);
     }
 
 
 
     @PatchMapping("/expanses2/{id}")
-    public ResponseEntity<String> editByIdAndUsername(@PathVariable Long id, @RequestBody ExpenseAddOrEditRequest expenseRequest, Authentication authentication) {
+    public String editByIdAndUsername(@PathVariable Long id, @RequestBody ExpenseAddOrEditRequest expenseRequest, Authentication authentication) {
         String username = (String) authentication.getPrincipal();
         Expense expense = new Expense(id, expenseRequest.getName(), expenseRequest.getAmount(),
                 expenseRequest.getDate(), expenseRequest.getComments(), expenseRequest.getLocation(), expenseRequest.getType(),
                 username, expenseCategoriesService.getCategoryById(username,Long.parseLong(expenseRequest.getCategory())), walletService.getWalletById(username, Long.parseLong(expenseRequest.getWallet())));
-        return ResponseEntity.ok().body(expenseService.editByIdAndUsername(id, expense, username));
+        return expenseService.editByIdAndUsername(id, expense, username);
     }
 
     @PostMapping("/expanses2")
-    public ResponseEntity<String> addTransaction(@RequestBody ExpenseAddOrEditRequest expenseRequest, Authentication authentication){
+    public String addTransaction(@RequestBody ExpenseAddOrEditRequest expenseRequest, Authentication authentication){
         String username = (String) authentication.getPrincipal();
         Expense expense = new Expense(expenseRequest.getName(), expenseRequest.getAmount(),
                 expenseRequest.getDate(), expenseRequest.getComments(), expenseRequest.getLocation(), expenseRequest.getType(),
                 username, expenseCategoriesService.getCategoryById(username,Long.parseLong(expenseRequest.getCategory())), walletService.getWalletById(username, Long.parseLong(expenseRequest.getWallet())));
-        return ResponseEntity.ok().body(expenseService.addTransaction(expense));
+        return expenseService.addTransaction(expense);
     }
 
-    @GetMapping("/test")
-    public void test(){
-        System.out.println("Real test hit");
-    }
 
     @GetMapping("/expanses2/topExpenseTransaction")
-    public ResponseEntity<Expense> getTopExpenseTransaction(Authentication authentication, @RequestParam("minDate") String minDate, @RequestParam("maxDate") String maxDate) throws ParseException {
+    public Expense getTopExpenseTransaction(Authentication authentication, @RequestParam("minDate") String minDate, @RequestParam("maxDate") String maxDate) throws ParseException {
         String username = (String) authentication.getPrincipal();
-        return ResponseEntity.ok().body(expenseService.getTopTransaction(username, minDate, maxDate, "Expense"));
+        return expenseService.getTopTransaction(username, minDate, maxDate, "Expense");
     }
 
     @GetMapping("/expanses2/topIncomeTransaction")
-    public ResponseEntity<Expense> getTopIncomeTransaction(Authentication authentication, @RequestParam("minDate") String minDate, @RequestParam("maxDate") String maxDate) throws ParseException {
+    public Expense getTopIncomeTransaction(Authentication authentication, @RequestParam("minDate") String minDate, @RequestParam("maxDate") String maxDate) throws ParseException {
         String username = (String) authentication.getPrincipal();
-        return ResponseEntity.ok().body(expenseService.getTopTransaction(username, minDate, maxDate, "Income"));
+        return expenseService.getTopTransaction(username, minDate, maxDate, "Income");
     }
 
 }
